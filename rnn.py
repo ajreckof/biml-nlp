49bc8354640cd73525c3aa8ba99118503e043733
MARTINEZ CHRISTOPHE p1709105 <christophe.martinez@etu.univ-lyon1.fr>
2023-10-17 16:16:42 +0200

    RNN a tester

--- rnn.py
+++ rnn.py
@@ -1,23 +1,29 @@
+import torch
 import torch.nn as nn
 
 class RNN(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
+    def __init__(self, input_size, embed_size, hidden_size, output_size):
         super(RNN, self).__init__()
 
         self.hidden_size = hidden_size
+        
+        self.i2e = nn.Linear(input_size, embed_size)
 
         self.i2o = nn.Sequential({
-            nn.Linear(input_size + hidden_size, hidden_size),
+            nn.Linear(input_size + embed_size + hidden_size, output_size),
             nn.ReLU()
             })
         self.i2h = nn.Sequential({
-            nn.Linear(hidden_size, output_size),
+            nn.Linear(input_size + embed_size + hidden_size, hidden_size),
             nn.ReLU()
             })
         self.softmax = nn.LogSoftmax(dim=1)
 
     def forward(self, input, hidden):
+        input = self.i2e(input)
+        
         combined = torch.cat((input, hidden), 1)
+        
         output = self.i2o(combined)
         output = self.softmax(output)
     
@@ -25,5 +31,5 @@ class RNN(nn.Module):
         
         return output, hidden
 
-    def initHidden(self):
-        return torch.zeros(1, self.hidden_size)
+    def initHidden(self, batch_size):
+        return torch.zeros(batch_size, self.hidden_size)
