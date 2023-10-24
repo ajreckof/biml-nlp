96920b5492572dd8f3793e5ab8ecf7f10f4c1752
ajreckof <tbonhoure@ymail.Com>
2023-10-24 14:19:56 +0200

    ca fonctionne mais ca fonctionne pas

--- projet.py
+++ projet.py
@@ -3,41 +3,93 @@ import torch
 import utils
 from torchtext.vocab import build_vocab_from_iterator
 import torch.nn.functional as F
+from torch.utils.data import DataLoader, TensorDataset
 from rnn import RNN
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
+from scipy import stats
+import copy
 
 #%%
-phrases, emotions = utils.load_file("dataset/train.txt")
+phrases_train, emotions_train = utils.load_file("dataset/train.txt")
+phrases_test, emotions_test = utils.load_file("dataset/test.txt")
+phrases_val, emotions_val = utils.load_file("dataset/val.txt")
+
+
 # %%
-vocab = build_vocab_from_iterator([[word for word in phrase] for phrase in phrases], specials=["<unk>"])
-# vocab.get_stoi()[phrases[0][1]]
-
+vocab = build_vocab_from_iterator([[word for word in phrase] for phrase in phrases_train], specials=["<unk>"])
 
 #%%
+
+stats.describe([len(phrase) for phrase in phrases_train])
+# longueur moyenne des phrases = 20
+# variance = 120
+# Longueur de phrase -> 30 mots
+
+#%%
+
+def prepare (phrases, sentence_length, emotions, stoi, labelEncoder):
+    print("start preparing data")
+    label_encoded_phrases = []
+    i = 0
+    s_unknown = stoi["<unk>"]
+    for phrase in phrases :
+        i+= 1
+        if i % 1000 == 0:
+            print(i)
+        label_encoded_phrases.append([stoi[word] if word in stoi else s_unknown  for word in phrase[:sentence_length]] + [s_unknown] * (sentence_length - len(phrase)))
+
+    print("preparing target")
+    # Préparation des données des émotions
+    label_encoded_emotions = labelEncoder.transform(emotions)
+
+    # Dataset
+    return TensorDataset(torch.tensor(label_encoded_phrases), torch.tensor(label_encoded_emotions))
+
+#%%
+
+# Préparation des données
+sentence_length = 30
+
+print("before label encoder")
 stoi = vocab.get_stoi()
-max = 0
-for phrase in phrases :
-    if len(phrase) > max:
-        max = len(phrase)
+labelEncoder = LabelEncoder()
+labelEncoder.fit(emotions_train)
+print("before prepare ")
+# Dataset d'entrainement
+train_dataset = prepare(phrases_train, sentence_length, emotions_train, stoi, labelEncoder)
+test_dataset = prepare(phrases_test, sentence_length, emotions_test, stoi, labelEncoder)
+
+#%%
+
+nb_epochs = 100
+batch_size = 25
+
+dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+rnn = RNN(input_size=len(vocab), embed_size=20, hidden_size=10, output_size=len(set(emotions_train)), batch_size=batch_size)
+loss_function = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(rnn.parameters(),lr=0.001)
+
+for i in range(nb_epochs):
+    print(i)
+    for sentences, emotions in dataloader:
+        optimizer.zero_grad()
+        y = rnn(sentences)
+        loss = loss_function(y, emotions)
+        loss.backward()
+        optimizer.step()
+    
+    sentences_test, emotions_test = test_dataset.tensors
+    correct = torch.argmax(rnn(sentences_test)) == emotions_test
+    correct_train_sum = 0
+    correct_train_len = 0
+    for sentences, emotions in dataloader:
+        correct_train = torch.argmax(rnn(sentences)) == emotions
+        correct_train_sum += sum(correct_train)
+        correct_train_len += len(correct_train)
+    print(float(correct_train_sum/correct_train_len))
+    print(float(sum(correct)/len(correct)))
+
         
-print([stoi[word] for word in phrases[0]])
-label_encoded_phrases = []
-i = 0
-for phrase in phrases :
-    i+= 1
-    if i % 1000 : 
-        print(i)
-    label_encoded_phrases.append([stoi[word] for word in phrase] + [stoi["<unk>"]] * (max - len(phrase)))
-print(len(label_encoded_phrases))
-label_encoded_phrases = torch.tensor(label_encoded_phrases)
-print(label_encoded_phrases)
-print(label_encoded_phrases[0:1].shape)
-
-print(label_encoded_phrases[0:1,0])
-
-
-# %%
-
-rnn = RNN(len(vocab), 128, 128, 6, 1)
-print(rnn(label_encoded_phrases[0:1,0]))
-
 # %%
--- rnn.py
+++ rnn.py
@@ -9,38 +9,44 @@ class RNN(nn.Module):
         self.input_size = input_size
         self.hidden_size = hidden_size
         
-        self.hidden = self.initHidden(batch_size)
         
         self.i2e = nn.Linear(input_size, embed_size, dtype=torch.double)
 
         self.i2o = nn.Sequential(
             nn.Linear(embed_size + hidden_size, output_size, dtype=torch.double),
-            nn.ReLU()
+            nn.Softmax(dim = 1)
             )
         self.i2h = nn.Sequential(
             nn.Linear(embed_size + hidden_size, hidden_size, dtype=torch.double),
-            nn.ReLU()
             )
-        self.softmax = nn.LogSoftmax(dim=1)
 
-    def forward(self, input):
+    def forward(self,input):
+
+        # Get the number of columns
+        batch_size, num_columns = input.shape
+
+        input = F.one_hot(input, num_classes=self.input_size).to(torch.double)
         
-        input = F.one_hot(input, num_classes=self.input_size)
-        input = input.to(torch.double)
-        
+        hidden = self.initHidden(batch_size)
+        rez = None
+        # Iterate over columns
+        for i in range(num_columns):
+            batch_word = input[:, i]
+            rez, hidden = self.forward_word(batch_word, hidden)
+        return rez
+
+    def forward_word(self, input, hidden):
+
         input = self.i2e(input)
         
-        combined = torch.cat((input, self.hidden), 1)
+        combined = torch.cat((input, hidden), 1)
         
         output = self.i2o(combined)
-        output = self.softmax(output)
     
-        self.hidden = self.i2h(combined)
+        hidden = self.i2h(combined)
         
-        return output
+        return output, hidden
 
     def initHidden(self, batch_size):
         return torch.zeros(batch_size, self.hidden_size, dtype=torch.double)
     
-    def resetHidden(self):
-        self.hidden = self.initHidden(self.hidden.shape[0])
